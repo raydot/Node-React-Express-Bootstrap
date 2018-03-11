2b6c28b81767eb63ef31c5d805e80daf7fe395b7
Dave Kanter <moatgoat@hotmail.com>
2018-03-11 12:37:51 -0700

    Basic app working!

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send("respond with a resource");
+
+  res.json([{
+  	id: 1,
+  	username: "dave"
+  }, {
+  	id: 2,
+  	username: "superdave"
+  }]);
 });
 
 module.exports = router;
